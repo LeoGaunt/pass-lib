94c728e894489bf7fc4e260f11bd796ea78739e8
Leo Gaunt <leogaunt05@outlook.com>
2020-10-02 18:00:15 +0100

    added more code

--- script.py
+++ script.py
@@ -8,34 +8,24 @@ import time
 
 #---Defines---
 
+userFileKey = ''
+passFileKey = ''
+
 def choice():
     choice = input('What do you want to do \n * Decode Username and Password (d) \n * Encode username and password (e) \n')
-    findKeys()
     if choice == 'e':
         encodeDB()
     else:
         decodeDB() 
 
-def findKeys():
-    if len(fileNames) > 1:
-        print('Found ', len(fileNames), ' keys.')
-        ''' May not need as different key for username and password
-        manyFiles = input('There are 2 key files ', fileNames,' which one would you like to use (Please choose a number between 1 and', len(fileNames))
-        fileChoice = fileNames[manyFiles]
-        '''
-    elif len(fileNames) == 0:
-        print('No Key Files found')
-        keyMake = input('Can you locate the files? (y/n)')
-        if keyMake == "y":
-            #Add option to find it theirselves
-            pass
-        else:
-            keyGen()
-    else:
-        print('Only One Key found. 2 Keys Needed to continue')
     
 def encodeDB():
-    pass
+    userKey = open(userFileKey, "r")
+    userKey.close()
+    passKey = open(passFileKey, "r")
+    passKey.close()
+
+
 
 def decodeDB():
     pass
@@ -51,13 +41,14 @@ def keyGen():
     file.write(userKey)  # The key is type bytes still
     file.close()
     name = input('What do you want your password keyfile to be called? ')
-    filename = name + '.key'
-    file = open(filename, 'wb')  # Open the file as wb to write bytes
+    filename1 = name + '.key'
+    file = open(filename1, 'wb')  # Open the file as wb to write bytes
     file.write(passKey)  # The key is type bytes still
     file.close()
     print('Taking back to home...')
     time.sleep(0.3)
     choice()
+    return filename, filename1
 
 
 
@@ -71,6 +62,31 @@ for x in range(0,len(files)):
         fileNames.append(files[x])
     else:
         pass
+
+if len(fileNames) > 2:
+    print('Found ', len(fileNames), ' keys.')
+    print('There are ', len(fileNames), ' key files ', fileNames,' which one would you like to use for the username (Please choose a number between 1 and', len(fileNames))
+    choiceFile = int(input())
+    userFileKey = fileNames[choiceFile-1]
+    print('There are ', len(fileNames), ' key files ', fileNames,' which one would you like to use for the password (Please choose a number between 1 and', len(fileNames))
+    choiceFile = int(input())
+    passFileKey = fileNames[choiceFile-1]
+    
+elif len(fileNames) == 0:
+    print('No Key Files found')
+    keyMake = input('Can you locate the files? (y/n)')
+    if keyMake == "y":
+        #Add option to find it theirselves
+        pass
+    else:
+        userFileKey = keyGen()[0]
+        passFileKey = keyGen()[1]
+elif len(fileNames) == 1:
+    print('Only One Key found. 2 Keys Needed to continue')
+    choice()
+else:
+    print('Found Keys')
+    #need to choose which key is which
 
 choice()
 
